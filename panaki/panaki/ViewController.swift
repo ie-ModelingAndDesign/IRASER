0754ab01a3f14206dd6c9a629296296afaf96af6
Kazuma <kazuma-arashi@hotmail.co.jp>
2015-11-05 15:31:48 +0900

    change single project

--- panaki/panaki/ViewController.swift
+++ panaki/panaki/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  DataViewController.swift
+//  ViewController.swift
 //  panaki
 //
 //  Created by Kazuma on 2015/11/05.
@@ -8,11 +8,7 @@
 
 import UIKit
 
-class DataViewController: UIViewController {
-
-    @IBOutlet weak var dataLabel: UILabel!
-    var dataObject: String = ""
-
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +18,6 @@ class DataViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        self.dataLabel!.text = dataObject
     }
 
 

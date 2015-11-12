f822b3ecf18fabadfd5ade6b35de00ca1d692c76
嘉数一輝 <e145704@ie.u-ryukyu.ac.jp>
2015-11-12 11:45:33 +0900

    test
    test

--- panaki/panaki/ViewController.swift
+++ panaki/panaki/ViewController.swift
@@ -12,8 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("test")
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
